caf0e543dc9160c6231210fe0f8165376a90b43a
lss6181 <131887316+lss6181@users.noreply.github.com>
2023-05-16 10:44:25 +0900

    Update app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://sparta:test@cluster0.bwpfqzd.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route("/reply", methods=["POST"])
+def guestbook_post():
+    reply_receive = request.form['reply_give']
+    doc = {
+        'reply':reply_receive
+    }
+    db.b4after.insert_one(doc)
+
+    return jsonify({'msg': '저장 완료!'})
+
+@app.route("/reply", methods=["GET"])
+def guestbook_get():
+    all_reply = list(db.b4after.find({},{'_id':False}))
+    return jsonify({'result': all_reply})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
